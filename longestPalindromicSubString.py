7a2d1142a7c6d7334b5fdc9be0f480eb8910d3eb
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-05-13 23:21:41 +0100

    Solution to the Longest Palindromic Sub-string

--- longestPalindromicSubString.py
+++ longestPalindromicSubString.py
@@ -14,3 +14,18 @@ Constraints:
 1 <= s.length <= 1000
 s consist of only digits and English letters. """
 
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand_around_center(s, left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+        
+        longest_palindrome = ""
+        for i in range(len(s)):
+            palindrome1 = expand_around_center(s, i, i)
+            palindrome2 = expand_around_center(s, i, i + 1)
+            longest_palindrome = max(longest_palindrome, palindrome1, palindrome2, key=len)
+        
+        return longest_palindrome
